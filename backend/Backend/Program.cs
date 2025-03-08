032add9a34c0dd8ef9b4dd320dbfc3e0d2fe2841
Márk Mihályi <mail@markmihalyi.hu>
2025-03-08 17:36:08 +0100

    Authentikáció implementálása, adatbázis és backend közti kapcsolat megvalósítása

--- backend/Backend/Program.cs
+++ backend/Backend/Program.cs
@@ -1,21 +1,57 @@
+﻿using Backend.Data;
+using Backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // DI IoC Container
+
+    // appsettings.json kialakítása fejlesztői környezettől függően
+    builder.Configuration.AddJsonFile(
+        $"Backend/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+        optional: false,
+        reloadOnChange: true
+     ).AddEnvironmentVariables();
+
+    // Adatbázis beállítása
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    // JWT authentikáció beállítása
+    string jwtKey = builder.Configuration["JWT:Secret"] ?? throw new InvalidOperationException("JWT Key is missing in configuration.");
+    string jwtIssuer = builder.Configuration["JWT:Issuer"] ?? throw new InvalidOperationException("JWT Issuer is missing in configuration.");
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtIssuer,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+            };
+        });
+
+    builder.Services.AddScoped<AuthService>();
+
+    builder.Services.AddControllers();
+
+    // https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
 }
 
-app.UseAuthorization();
+var app = builder.Build();
+{
+    // Middleware lánc
 
-app.MapControllers();
+    if (app.Environment.IsDevelopment()) app.MapOpenApi();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+}
 
 app.Run();
