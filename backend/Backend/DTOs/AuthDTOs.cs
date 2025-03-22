1eed3365133e14ddba8dafa4ece7f6c34834df79
Márk Mihályi <mail@markmihalyi.hu>
2025-03-22 20:46:02 +0100

    Authentikáció átalakítása cookie alapú token továbbításra, kezelésre + logout endpoint

--- backend/Backend/DTOs/AuthDTOs.cs
+++ backend/Backend/DTOs/AuthDTOs.cs
@@ -1,10 +1,4 @@
 ﻿namespace Backend.DTOs
 {
     public record AuthRequestBody(string Username, string Password);
-
-    public record LoginResponse(string Message, string AccessToken, string RefreshToken) : ApiResponse(true, Message);
-
-    public record TokenRequestBody(string RefreshToken);
-
-    public record TokenResponse(string Message, string AccessToken) : ApiResponse(true, Message);
 }
