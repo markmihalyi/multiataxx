790161b41e17ef5ada26ee3781df4e5a3bebc979
markmihalyi <mail@markmihalyi.hu>
2025-03-31 19:51:29 +0200

    fix: distance calculation

--- backend/Backend/Game/Logic/MoveValidator.cs
+++ backend/Backend/Game/Logic/MoveValidator.cs
@@ -6,13 +6,7 @@ namespace Backend.GameLogic.Logic
     {
         public int DistanceTo(Point other)
         {
-            int dx = Math.Abs(other.X - X);
-            int dy = Math.Abs(other.Y - Y);
-
-            if (dx + dy > 2)
-                return dx + dy;
-
-            return Math.Max(dx, dy);
+            return Math.Max(Math.Abs(other.X - X), Math.Abs(other.Y - Y));
         }
     }
 
