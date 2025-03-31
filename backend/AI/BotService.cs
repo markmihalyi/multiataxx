8ee68f5b199af4672f6fcf9be9f3dbd167f89301
LaczyG <gaborlaczy@gmail.com>
2025-03-31 15:24:58 +0200

    Minimax implementálása

--- backend/AI/BotService.cs
+++ backend/AI/BotService.cs
@@ -1,61 +1,46 @@
-﻿using System.Numerics;
+﻿
+using System.Numerics;
 
 public class BotService
 {
     private GameState _gameState;
-
+    private Minimax _minimax;
     public BotService(GameState gameState)
     {
         _gameState = gameState;
+        _minimax = new Minimax(gameState, 3);
     }
 
     // Bot lépés generálása
     // Jelenleg csak random (egyszerű)
     public (int x, int y, int fromx, int fromy) GenerateBotMove()
     {
-        Random rand = new Random();
+        // TODO: Booster metódus implementálása
 
-        var botPieces = _gameState.GetPlayerPieces(2).ToList();
-        if (botPieces.Count == 0) throw new Exception("A botnak nincs elérhető lépése!");
-
-        (int fromx, int fromy) = botPieces[rand.Next(botPieces.Count)]; // Véletlen bábu kiválasztása
-        // Megkeressük az összes lehetséges célmezőt
-        List<(int x, int y)> possibleMoves = new List<(int, int)>();
-
-        int[] dx = { -1, 1, 0, 0, -1, 1, -1, 1, -2, 2, 0, 0 };
-        int[] dy = { 0, 0, -1, 1, -1, 1, 1, -1, 0, 0, -2, 2 };
-
-        for (int i = 0; i < dx.Length; i++)
-        {
-            int newX = fromx + dx[i];
-            int newY = fromy + dy[i];
-
-            if (newX >= 0 && newX < _gameState.Board.GetLength(0) &&
-                newY >= 0 && newY < _gameState.Board.GetLength(1) &&
-                _gameState.IsValidMove(newX, newY, fromx, fromy))
-            {
-                possibleMoves.Add((newX, newY));
-            }
-        }
-        // Ha nincs érvényes lépés, próbálunk egy másik bábut
-        if (possibleMoves.Count == 0) return GenerateBotMove();
-
-        // Véletlenszerű érvényes célmező kiválasztása
-        (int x, int y) = possibleMoves[rand.Next(possibleMoves.Count)];
-        return (x, y, fromx, fromy);
+        return (0, 0, 0, 0);
     }
 
 
     // Bot lépése végrehajtása és kiírása
     public void MakeBotMove()
     {
+        var move = _minimax.MaxMove(_gameState);
         // Generáljuk a bot lépését
-        var move = GenerateBotMove();
         int x = move.x;
         int y = move.y;
         int fromx = move.fromx;
         int fromy = move.fromy;
         // A bot lépése végrehajtása
+        // Ha a bot nem talált érvényes lépést
+        if (x == -1 && y == -1 && fromx == -1 && fromy == -1)
+        {
+            Console.WriteLine("A bot nem tudott lépni!");
+            return;
+        }
+
+        // A bot lépése végrehajtása
         _gameState.MakeMove(x, y, fromx, fromy);
+        Console.WriteLine($"Bot moved from actually ({fromx}, {fromy}) to ({x}, {y})");
+        Thread.Sleep(2000);
     }
 }
--- backend/AI/Minimax.cs
+++ backend/AI/Minimax.cs
@@ -1,7 +1,119 @@
-﻿namespace AI
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Numerics;
+
+public class Minimax
 {
-    class Minimax
+    private GameState _gameState; 
+    private int _maxDepth;
+
+    public Minimax(GameState gameState, int maxDepth)
     {
-        //Minimax algoritmus
+        _gameState = gameState; 
+        _maxDepth = maxDepth;
     }
+
+    public int MinimaxAlgorithm(GameState gameState, int depth, bool isMaxPlayer, int alpha, int beta)
+    {
+        if (depth <= 0 || GameOver(gameState))
+        {
+            return Evaluate(gameState);
+        }
+
+        if (isMaxPlayer)
+        {
+
+            // Maximalizálni kívánt ág
+            int maxEval = int.MinValue;
+            gameState.SwitchPlayer();
+            foreach (var move in gameState.GeneratePossibleMoves(2))
+            {
+                Console.WriteLine($"BOT Lépés: [{move.fromx}, {move.fromy}] -> [{move.x}, {move.y}]");
+                GameState newState = gameState.Clone();
+                newState.MakeMove(move.x, move.y, move.fromx, move.fromy);
+                int eval = MinimaxAlgorithm(newState, depth - 1, !isMaxPlayer, alpha, beta);
+                maxEval = Math.Max(maxEval, eval);
+                alpha = Math.Max(alpha, eval);
+
+                if (beta <= alpha)
+                {
+                    Console.WriteLine("KILÉP ALPHA");
+                    break;  // Metszés
+                }
+
+            }
+            return maxEval;
+        }
+        else
+        {
+
+            int minEval = int.MaxValue;
+            gameState.SwitchPlayer();
+            foreach (var move in gameState.GeneratePossibleMoves(1))
+            {
+                Console.WriteLine($"Játékos Lépés: [{move.fromx}, {move.fromy}] -> [{move.x}, {move.y}]");
+                GameState newState = gameState.Clone();
+                newState.MakeMove(move.x, move.y, move.fromx, move.fromy);
+                int eval = MinimaxAlgorithm(newState, depth - 1, !isMaxPlayer, alpha, beta);
+                minEval = Math.Min(minEval, eval);
+                beta = Math.Min(beta, eval);  // Frissítjük a beta értéket
+
+                if (beta <= alpha)
+                {
+                    Console.WriteLine("KILÉP BETA");
+                    break;  // Metszés
+                }
+
+            }
+            return minEval;
+        }
+    }
+    public (int x, int y, int fromx, int fromy) MaxMove(GameState gameState)
+    {
+        (int x, int y, int fromx, int fromy) bestMove = (-1, -1, -1, -1); // Hibás lépés alapértéke
+        int maxEval = int.MinValue; // - végtelen
+        int alpha = int.MinValue;
+        int beta = int.MaxValue;
+        foreach (var move in gameState.GeneratePossibleMoves(2))
+        {
+            GameState newState = gameState.Clone(); 
+            newState.MakeMove(move.x, move.y, move.fromx, move.fromy);
+            int eval = MinimaxAlgorithm(newState, _maxDepth - 1, false, alpha, beta);
+            if (eval > maxEval)
+            {
+                maxEval = eval;
+                bestMove = move;
+                Console.WriteLine($"Frissített maxEval: {maxEval}, Lépés: [{move.fromx}, {move.fromy}] -> [{move.x}, {move.y}]");
+                Thread.Sleep(2000);
+            }
+        }
+
+        return bestMove; // Ha nem talált érvényes lépést, (-1, -1, -1, -1) értéket ad vissza
+}
+
+
+    public Boolean GameOver(GameState gameState)
+    {
+        if (gameState.GeneratePossibleMoves(1).Count() == 0 || gameState.GeneratePossibleMoves(2).Count() == 0)
+        {
+            Console.WriteLine("Game Over állapot elérve!");
+            return true;
+        }
+        return false;
+    }
+    // Lehetséges lépések generálása
+   
+
+
+    // Kiértékelő
+    private int Evaluate(GameState gameState)
+    {
+        // TODO: Jobb kiértékelő függvény 
+        int botScore = gameState.GetPlayerPieces(2).Count();
+        int playerScore = gameState.GetPlayerPieces(1).Count();
+
+        return botScore - playerScore;
+    }
+
 }
