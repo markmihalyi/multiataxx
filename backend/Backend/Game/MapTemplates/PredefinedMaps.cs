a897de7f43c8e823b2b0969e457e8e4e99a1d861
markmihalyi <contact@markmihalyi.hu>
2025-05-03 22:46:27 +0200

    fix predefined maps

--- backend/Backend/Game/MapTemplates/PredefinedMaps.cs
+++ backend/Backend/Game/MapTemplates/PredefinedMaps.cs
@@ -7,19 +7,19 @@ namespace Backend.GameLogic.MapTemplates
         public static readonly CellState[,] SmallMap = new CellState[(int)BoardSize.Small, (int)BoardSize.Small]
         {
             { CellState.Player1, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
-            { CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
-            { CellState.Empty, CellState.Empty, CellState.Wall, CellState.Empty, CellState.Empty },
-            { CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
-            { CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Player2 }
+            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
+            { CellState.Empty,   CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty },
+            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
+            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Player2 }
         };
 
         public static readonly CellState[,] MediumMap = new CellState[(int)BoardSize.Medium, (int)BoardSize.Medium]
         {
             { CellState.Player1, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
-            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty },
+            { CellState.Empty,   CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Player2 }
         };
@@ -29,9 +29,9 @@ namespace Backend.GameLogic.MapTemplates
             { CellState.Player1, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty },
-            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
+            { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty, CellState.Empty, CellState.Wall,  CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty },
             { CellState.Empty,   CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Empty, CellState.Player2 }
