a239764307210d7f6202032700674150e1c3bba3
GalaxySer <seresdaniel@edu.bme.hu>
2025-05-06 20:19:44 +0200

    Loading animation

---
+++ frontend/src/components/ThreeDotLoading.tsx
@@ -0,0 +1,11 @@
+import "../styles/ThreeDotLoading.css";
+
+export default function ThreeDotLoading() {
+	return (
+		<div className="dot-loader">
+			<span className="dot" />
+			<span className="dot" />
+			<span className="dot" />
+		</div>
+	);
+}
